aa6b720854fb2f89bdc25819866df6bccdad9b7a
aragaer <aragaer@gmail.com>
2017-10-13 00:04:01 +0300

    Rewire main.py to use old telegram bot instead

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 
+import atexit
 import logging
+import os
+import socket
 import subprocess
 import sys
 import time
 
-from router.routing import Faucet, Router, PipeFaucet, Sink, PipeSink, Rule
+from router.routing import Faucet, Router, PipeFaucet, Sink, PipeSink, Rule, SocketFaucet
+
+
+class OldTgFaucet(SocketFaucet):
+
+    def __init__(self, sock, owner_id):
+        super().__init__(sock)
+        self._owner = owner_id
+        sock.send(b'register backend\n')
+
+    def read(self):
+        """Almost exact copy of SocketFaucet.read"""
+        pos = self._buf.find("\n")
+        if pos == -1:
+            try:
+                data = self._sock.recv(4096).decode()
+                if not data:
+                    raise EndpointClosedException()
+                self._buf += data
+            except BlockingIOError:
+                pass
+            pos = self._buf.find("\n")
+        if pos == -1:
+            return
+        line, self._buf = self._buf[:pos], self._buf[pos+1:]
+        if line.startswith("message:"):
+            line = line[8:].strip()
+        return {"from": {"media": "telegram", "id": self._owner},
+                "text": line}
 
 
 class TgFaucet(Faucet):
@@ -42,31 +73,48 @@ class DumpSink(Sink):
         self._logger.debug("Dropped %s", message)
 
 
-def main(owner_id):
+def main(owner_id, sock_name):
     logger = logging.getLogger("router")
-    tg_proc = subprocess.Popen([".env/bin/python3", "single_stdio.py"],
-                               bufsize=1,
-                               cwd="tg",
-                               stdout=subprocess.PIPE,
-                               stdin=subprocess.PIPE)
-
-    faucet = TgFaucet(PipeFaucet(tg_proc.stdout.fileno()))
     router = Router(DumpSink('dumped'))
     router.add_sink(DumpSink('seen'), 'from_me')
+
+    if False:
+        tg_proc = subprocess.Popen([".env/bin/python3", "single_stdio.py"],
+                                bufsize=1,
+                                cwd="tg",
+                                stdout=subprocess.PIPE,
+                                stdin=subprocess.PIPE)
+
+        faucet = TgFaucet(PipeFaucet(tg_proc.stdout.fileno()))
+    else:
+        tg_proc = subprocess.Popen([".env/bin/python3", "pa.py"],
+                                   bufsize=1,
+                                   cwd="tg")
+        sock = socket.socket(socket.AF_UNIX)
+        path = "tg/"+sock_name
+        logging.getLogger("main").info("connecting to %s", path)
+        while not os.path.exists(path):
+            time.sleep(1)
+        sock.connect(path)
+        faucet = OldTgFaucet(sock, owner_id)
+    atexit.register(tg_proc.terminate)
     router.add_faucet(faucet, "tg")
     router.add_rule(Rule('from_me', id=owner_id), 'tg')
+
     while True:
         router.tick()
         time.sleep(1)
 
 
 if __name__ == '__main__':
+    sock_name = "socket"
     with open('tg/token.txt') as token_file:
         for line in token_file:
             key, value = line.split()
             if key == 'OWNER':
                 owner_id = int(value)
-                break
+            elif key == 'SOCKET':
+                sock_name = value.strip()
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     logging.getLogger("dump").setLevel(logging.DEBUG)
-    main(owner_id)
+    main(owner_id, sock_name)
