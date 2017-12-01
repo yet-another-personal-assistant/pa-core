6c0dd9e647aa1c2856a43915121f26379376b620
aragaer <aragaer@gmail.com>
2017-12-01 15:46:20 +0300

    Extract tg-related stuff to a separate file

--- main.py
+++ main.py
@@ -14,38 +14,9 @@ import yaml
 
 from router.routing import Faucet, Router, PipeFaucet, Sink, Rule
 from router.routing.runner import Runner
+from tg import TgFaucet, TgSink, TelegramToBrainRule
 
 _LOGGER = logging.getLogger(__name__)
-
-
-class TgFaucet(Faucet):
-
-    def __init__(self, base_faucet):
-        super().__init__()
-        self._base = base_faucet
-
-    def read(self):
-        event = self._base.read()
-        if event and 'message' in event:
-            message = event['message']
-            if 'chat' in message and message['chat'].get('type') == "private":
-                message['from']['media'] = 'telegram'
-                return message
-
-
-class TgSink(Sink):
-
-    def __init__(self, base_sink):
-        super().__init__()
-        self._base = base_sink
-        self._logger = logging.getLogger("tg_sink")
-
-    def write(self, message):
-        if 'chat_id' in message:
-            self._base.write(message)
-        else:
-            self._logger.warning("No chat_id set for message [%s]",
-                                 message['text'])
 
 
 class StdinFaucet(Faucet):
@@ -74,19 +45,6 @@ class StdoutSink(Sink):
 
     def write(self, message):
         print("{}: {}".format(self._name, message['text']))
-
-
-class TelegramToBrainRule(Rule):
-
-    def __init__(self, tg_users):
-        super().__init__(target="brain", media="telegram")
-        self._len += 1
-        self._tg_users = {**tg_users}
-
-    def target_for(self, message):
-        source = message['from']
-        if source.get('media') == "telegram" and "id" in source:
-            return self._tg_users.get(source['id'])
 
 
 def make_brain_factory(configs, runner):
---
+++ tg.py
@@ -0,0 +1,46 @@
+import logging
+
+from router.routing import Faucet, Sink, Rule
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class TgFaucet(Faucet):
+
+    def __init__(self, base_faucet):
+        super().__init__()
+        self._base = base_faucet
+
+    def read(self):
+        event = self._base.read()
+        if event and 'message' in event:
+            message = event['message']
+            if 'chat' in message and message['chat'].get('type') == "private":
+                message['from']['media'] = 'telegram'
+                return message
+
+
+class TgSink(Sink):
+
+    def __init__(self, base_sink):
+        super().__init__()
+        self._base = base_sink
+
+    def write(self, message):
+        if 'chat_id' in message:
+            self._base.write(message)
+        else:
+            _LOGGER.warning("No chat_id set for message [%s]", message['text'])
+
+
+class TelegramToBrainRule(Rule):
+
+    def __init__(self, tg_users):
+        super().__init__(target="brain", media="telegram")
+        self._len += 1
+        self._tg_users = tg_users.copy()
+
+    def target_for(self, message):
+        source = message['from']
+        if source.get('media') == "telegram" and "id" in source:
+            return self._tg_users.get(source['id'])
