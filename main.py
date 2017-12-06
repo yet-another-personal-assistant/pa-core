34344ab9540c4ebc772e00b792f3c7e227cafc8f
Ilya Konovalov <ikonovalov@topcon.com>
2017-12-06 19:46:48 +0300

    Set up brain persistence files.
    Pass additional flags to brain. Also prepare the directory for brain dumps.

--- main.py
+++ main.py
@@ -19,16 +19,19 @@ from tg import TgFaucet, TgSink, TelegramToBrainRule
 _LOGGER = logging.getLogger(__name__)
 
 
-def make_brain_factory(configs, runner):
+def make_brain_factory(configs, runner, args):
     def instantiate_brain(router, brain_name):
         _LOGGER.debug("Checking if we should wake up brain %s", brain_name)
         config = configs.get(brain_name)
         if config:  # it isn't started, that's why we are here
+            sock_path = os.path.join(args.brains, brain_name)
+            saved_path = os.path.join(args.brains, config.user_name)
             runner.ensure_running("brain",
                                   alias=brain_name,
-                                  with_args=["--socket", brain_name,
-                                             "--config", config.filename],
-                                  socket=os.path.join("brain", brain_name))
+                                  with_args=["--socket", sock_path,
+                                             "--config", config.filename,
+                                             "--saved", saved_path],
+                                  socket=sock_path)
             router.add_sink(runner.get_sink(brain_name), brain_name)
             router.add_faucet(runner.get_faucet(brain_name), brain_name)
     return instantiate_brain
@@ -42,8 +45,9 @@ class DumpSink(Sink):
 
 class UserConfig:
 
-    def __init__(self, filename):
+    def __init__(self, user_name, filename):
         self._filename = os.path.abspath(filename)
+        self._user_name = user_name
         with open(filename) as user_config:
             self._config = yaml.load(user_config)
 
@@ -62,6 +66,10 @@ class UserConfig:
     @property
     def incoming(self):
         return self._config.get('incoming')
+
+    @property
+    def user_name(self):
+        return self._user_name
 
 
 class NotifierSink(Sink):
@@ -113,8 +121,9 @@ def build_router(args):
         brain_name = 'brain{}'.format(num)
         if not user_file_name.endswith(".yml"):
             continue
+        user_name = user_file_name[:-4]
         file_path = os.path.join(args.users, user_file_name)
-        config = UserConfig(file_path)
+        config = UserConfig(user_name, file_path)
         if config.telegram is not None:
             tg_users[config.telegram] = brain_name
         if config.local:
@@ -122,7 +131,7 @@ def build_router(args):
         if config.incoming is not None:
             add_incoming_faucet(router, brain_name, config.incoming)
         configs[brain_name] = config
-    router.add_sink_factory(make_brain_factory(configs, runner))
+    router.add_sink_factory(make_brain_factory(configs, runner, args))
 
     if args.token is not None:
         add_tg_endpoint(router, runner, tg_users, args)
@@ -174,6 +183,9 @@ def main():
     args.name = config.get('name')
     args.translator = config.get('translator')
     args.notifications = config.get('notify')
+    args.brains = os.path.abspath('./_brains')
+    if not os.path.exists(args.brains):
+        os.mkdir(args.brains)
 
     debug = config.get('debug', {})
     for module, log_level in _LOG_LEVELS_DEFAULT.items():
